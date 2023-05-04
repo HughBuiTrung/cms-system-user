27e59131a57ad96e4abf4de476ebed315327a0ae
Tony Nguyen <nhattruong1811@gmail.com>
2023-05-04 20:20:24 +0700

    add env:

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 // routes
 const todosRoutes = require('./routes/todos');
 
+dotenv.config();
 app.use(cors());
 app.use(express.static('public'));
 
 // connect to DB
 mongoose
-  .connect('xxx', {
+  .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
   })
   .catch((error) => console.log("Connect Fail: ", error));
@@ -24,7 +26,7 @@ app.get('/', (_, res) => res.send('API running...'))
 app.use('/api/todo', todosRoutes);
 
 // listen apps
-const PORT = 4002;
+const PORT = process.env.PORT || 4002;
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`)
 })
